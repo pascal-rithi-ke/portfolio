dacf7fc506acd296db45475b7c8d1900dc44b895
Pascal Kien <pascal.rithi@gmail.com>
2022-04-02 00:14:33 +0200

    solve scrollbar problem

--- src/container/App.js
+++ src/container/App.js
@@ -1,13 +1,13 @@
 import Experience from "../component/Experience";
-import photo from "../images/photo.jpg"
+import photo from "../images/photo.jpg";
 
 function App() {
   return (
-    <div className="light presentation">
-      <h2>I'm Kien<br/>Eng Pascal<br/>A fullstack developer</h2>
+    <div className="presentation">
+      <h2 className="light">I'm Kien<br/>Eng Pascal<br/>A fullstack developer</h2>
       <img className="profil_photo" src={photo}/>
-      <div className="wave"></div>
-      <Experience/>
+      <div className="wave light"></div>
+      <Experience className="light"/>
     </div>
   );
 }

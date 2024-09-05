2a51dd1a6d78a155c84151bc6232440168ba60ea
Pascal-ke <pascal.rithi@gmail.com>
2024-09-05 02:13:25 +0200

    setup shadcn/ui to modernized

---
+++ src/data/info.js
@@ -0,0 +1,271 @@
+export const data = {
+    "project":[
+        {
+            title: "Weapon detection",
+            subtitle: "Python YOLOv3",
+            description: "Create a weapon detection on a video or by image",
+            type: ["IA", "Big Data"],
+            tech: ["Python", "YOLOv3"],
+            img: require("../assets/weapon_dectection.png"),
+            link: "https://github.com/pascal-rithi-ke/detection_arme",
+        },
+        {
+            title: "Wasteland Warrior",
+            subtitle: "React Native Game",
+            description: "Interactive storytelling games with multiple choices",
+            type: ["Mobile", "Web"],
+            tech: ["React Native"],
+            img: "https://raw.githubusercontent.com/pascal-rithi-ke/Wasteland_Warrior/master/front/assets/game/Avec_Chien.png",
+            link: "https://github.com/pascal-rithi-ke/Wasteland_Warrior",
+        },
+        {
+            title: "ChatBot disease detection",
+            subtitle: "Python Flask React Spacy",
+            description: "Create a chatbot that can detect diseases with NLP and Machine Learning",
+            type: ["IA", "Web"],
+            tech: ["Python", "Flask", "React", "Spacy"],
+            img: require("../assets/chatbot_medical.jpeg"),
+            link: "https://detection-maladie-5y6v-iwt84fwzt-pascal-rithi-kes-projects.vercel.app/",
+        },
+        {
+            title: "Data architecture",
+            subtitle: "Kafka Cassandra Docker",
+            description: "Récupérer des données en temps réel et les stocker dans une base de données NoSQL",
+            type: ["Big Data"],
+            tech: ["Kafka", "Cassandra", "Docker"],
+            img: require("../assets/big_data.jpg"),
+            link: "https://github.com/AdrienMirabelle/Projet_Architecture_Data",
+        },
+        {
+            title: "E-motion",
+            subtitle: "Symfony Stripe Project",
+            description: "Create a E-commerce website with a login and payment system",
+            type: ["Web", "Payment System"],
+            tech: ["Symfony", "Stripe"],
+            img: require("../assets/Stripe_logo.png"),
+            link: "https://github.com/Dalsaba/E-Motion/",
+        },
+        {
+            title: "E-commerce",
+            subtitle: "Symfony Project",
+            description: "Create a CRUD E-commerce website with a login system for users",
+            type: ["Web"],
+            tech: ["Symfony"],
+            img: require("../assets/symfony.png"),
+            link: "https://github.com/bflorestal/e-commerce",
+        },
+        {
+            title: "Pokedex",
+            subtitle: "React Api(pokeApi)",
+            description: "Create a Pokedex with a SearchBar to display more informations",
+            type: ["Web"],
+            tech: ["React", "API"],
+            img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/512px-Pok%C3%A9_Ball_icon.svg.png",
+            link: "https://github.com/pascal-rithi-ke/pokedex",
+        },
+        {
+            title: "MangaInfo",
+            subtitle: "React Native MongoDB Python",
+            description: "Self-taught project, show more informations by clicking on button from a list of manga",
+            type: ["Mobile", "Web"],
+            tech: ["React Native", "MongoDB", "Python"],
+            img: "https://i.pinimg.com/736x/56/93/dc/5693dcd41e4d36d6ab97a33a53c3a381.jpg",
+            link: "https://github.com/pascal-rithi-ke/MangaInfo",
+        },
+        {
+            title: "Portfolio",
+            subtitle: "React",
+            description: "Create and deploy my portfolio with git pages",
+            type: ["Web"],
+            tech: ["React"],
+            img: "https://png.pngitem.com/pimgs/s/533-5339521_briefcase-hd-png-download.png",
+            link: "https://github.com/pascal-rithi-ke/portfolio",
+        },
+        {
+            title: "RealTime Chat",
+            subtitle: "Node Express Socket.io",
+            description: "Create a realtime chat application and can be used on web & desktop with electron",
+            type: ["Web", "Desktop"],
+            tech: ["Node", "ExpressJS", "Socket.io"],
+            img: "https://miro.medium.com/max/700/1*DfmKdD8EwFzmpapP3lWj5Q.png",
+            link: "https://github.com/pascal-rithi-ke/socket.io",
+        },
+        {
+            title: "Calendar",
+            subtitle: "Node Express MongoDB",
+            description: "Create a todolist that displays tasks on a calendar",
+            type: ["Web"],
+            tech: ["Node", "Express", "MongoDB"],
+            img: "https://thumbs.dreamstime.com/b/vecteur-de-symbole-bloc-notes-contour-d-un-avec-stylo-dans-l-avant-sur-le-fond-blanc-format-142213550.jpg",
+            link: "https://github.com/pascal-rithi-ke/calendar",
+        },
+        {
+            title: "SlowMotion",
+            subtitle: "MongoDB Python Docker",
+            description: "Create a video game review application with React",
+            type: ["Web"],
+            tech: ["MongoDB", "Python", "Docker"],
+            img: "https://explora.saint-etienne.fr/wp-content/uploads/2021/01/jeux-video-1280x1024.jpg",
+            link: "https://github.com/pascal-rithi-ke/slowmotion/tree/master/jeuxvideo",
+        },
+        {
+            title: "La Bibliothèque",
+            subtitle: "PHP SQL",
+            description: "Create an online library with a book reservation and a login system for users",
+            type: ["Web"],
+            tech: ["PHP", "SQL"],
+            img: "https://www.freeiconspng.com/thumbs/book-icon/book-icon-black-good-galleries--24.jpg",
+            link: "https://github.com/pascal-rithi-ke/Biblio",
+        },
+    ],
+    "experience":[
+        {
+            title:"ZooLemag",
+            subtitle: "Developer Fullstack PHP/JS/SQL",
+            description: "Apprenticeship, maintenance of the website and development of new features",
+            img: "https://zoolemag.com/img/brand/logo.png",
+            date: "Sept 2022 - Sept 2024"
+        },
+        {
+            title:"Safran",
+            subtitle: "Developer SharePoint and Python",
+            description: "Apprenticeship: Manage department SharePoints, develop Python solutions.",
+            img: "https://pbs.twimg.com/profile_images/920279256101478405/xrgf2ci8_400x400.jpg",
+            date: "Oct 2021 - Sept 2022"
+        },
+        {
+            title:"Okatro",
+            subtitle: "PHP - IIS",
+            description: "Internship, create an intranet for a middle school using PHP and IIS",
+            img: "https://media.licdn.com/dms/image/v2/C560BAQGo-KExUmlWhA/company-logo_200_200/company-logo_200_200/0/1630614927626?e=2147483647&v=beta&t=04TIGd8jI371yuNkhAR4SCNuOJSsrO8ktDn1FsAS2-A",
+            date: "Mar 2021 -  May 2021"
+        },
+        {
+            title:"Bloasis",
+            subtitle: "Web consultant",
+            description: "InternShip, web consulting to creat a website booking rooms",
+            img: "https://cnstlltn.com/300x200/f191cd7b-0e94-47f6-8625-05c7be0d8bb1/logo+bloasis+fond+orange+canva.png",
+            date : "May 2020 - Jun 2020"
+        }
+    ],
+    "tech_lang":[
+        {
+            title:"HTML",
+            img: require("../assets/html-5.png")
+        },
+        {
+            title:"CSS",
+            img: require("../assets/css-3.png")
+        },
+        {
+            title:"JS",
+            img: require("../assets/js.png")
+        },
+        {
+            title:"PHP",
+            img: require("../assets/php.png")
+        },
+        {
+            title:"Python",
+            img: require("../assets/python.png")
+        },
+    ],
+    "tech_framework":[
+        {
+            title:"React / React Native",
+            img: require("../assets/react.png")
+        },
+        {
+            title:"Symfony",
+            img: require("../assets/symfony.png")
+        },
+        {
+            title:"Django",
+            img: require("../assets/django.jpg")
+        },
+        {
+            title:"Tailwind",
+            img: require("../assets/tailwind.png")
+        },
+        {
+            title:"Bootstrap",
+            img: require("../assets/bootstrap.png")
+        },
+    ],
+    "tech_bdd":[
+        {
+            title:"SQL",
+            img: require("../assets/sql.png")
+        },
+        {
+            title:"MongoDB",
+            img: "https://w7.pngwing.com/pngs/216/509/png-transparent-mongodb-node-js-npm-open-source-model-angularjs-leaf-leaf-logo-grass.png"
+        },
+    ],
+    "tech_tool":[
+        {
+            title:"VirtualBox",
+            img: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Virtualbox_logo.png"
+        },
+        {
+            title:"GitHub",
+            img: require("../assets/github.png")
+        },
+        {
+            title:"Bitbucket",
+            img: require("../assets/bitbucket.jpg")
+        },
+        {
+            title:"Jira",
+            img: require("../assets/jira.png")
+        },
+        {
+            title:"Docker",
+            img: require("../assets/docker.png")
+        },
+        {
+            title:"VS Code",
+            img: require("../assets/vs_code.png")
+        },
+    ],
+    "tech_cloud":[
+        {
+            title:"AWS",
+            img: require("../assets/aws.png")
+        },
+        {
+            title:"GCP",
+            img: require("../assets/gcp.png")
+        },
+        {
+            title:"Azure",
+            img: require("../assets/azure.png")
+        },
+    ],
+    "type_project":[
+        {
+            title:"IA",
+            color : "bg-blue-500"
+        },
+        {
+            title:"Mobile",
+            color : "bg-green-500"
+        },
+        {
+            title:"Web",
+            color : "bg-yellow-500"
+        },
+        {
+            title:"Big Data",
+            color : "bg-red-500"
+        },
+        {
+            title:"Desktop",
+            color : "bg-indigo-500"
+        },
+        {
+            title:"Payment System",
+            color : "bg-purple-500"
+        }
+    ]
+}
